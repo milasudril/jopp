8b8b4a85c55ddbc67ac44edb9580c6f9254e0f7e
Torbjörn Rathsman <milasudril@gmail.com>
2023-04-22 08:25:50 +0200

    Test to_number

--- lib/parser.test.cpp
+++ lib/parser.test.cpp
@@ -3,3 +3,28 @@
 #include "./parser.hpp"
 
 #include <testfwk/testfwk.hpp>
+
+TESTCASE(jopp_parser_to_number)
+{
+	auto const too_big = jopp::to_number("1e400");
+	EXPECT_EQ(too_big.has_value(), false);
+
+	auto const inf = jopp::to_number("inf");
+	EXPECT_EQ(inf, std::numeric_limits<jopp::number>::infinity());
+
+	auto const negative_inf = jopp::to_number("-inf");
+	EXPECT_EQ(negative_inf, -std::numeric_limits<jopp::number>::infinity());
+
+	auto const nan = jopp::to_number("nan");
+	REQUIRE_EQ(nan.has_value(), true);
+	EXPECT_EQ(std::isnan(*nan), true);
+
+	auto const finite_number = jopp::to_number("4.0");
+	EXPECT_EQ(finite_number, 4.0);
+
+	auto const junk_after_number = jopp::to_number("4.0eiruseo");
+	EXPECT_EQ(junk_after_number.has_value(), false);
+
+	auto const junk_before_number = jopp::to_number("aseiorui4.0");
+	EXPECT_EQ(junk_before_number.has_value(), false);
+}
