aaa888fdaa8bdaf869d87f5bd83e8f5a25cdcc4c
Torbjörn Rathsman <milasudril@gmail.com>
2023-04-26 21:26:35 +0200

    Initial work on serializer

---
+++ lib/serializer.hpp
@@ -0,0 +1,45 @@
+#ifndef JOPP_SERIALIZER_HPP
+#define JOPP_SERIALIZER_HPP
+
+#include "./types.hpp"
+#include "./delimiters.hpp"
+
+#include <string_view>
+#include <stack>
+#include <span>
+#include <reference_wrapper>
+
+namespace jopp
+{
+	enum class serializer_error_code
+	{
+		completed,
+		buffer_is_full,
+		illegal_char_in_string
+	};
+
+	struct serialize_result
+	{
+		value const* last_value;
+		serializer_error_code ec;
+	};
+
+	struct serailzer_context
+	{
+
+	};
+
+	class serializer
+	{
+	public:
+		explicit serializer(std::reference_wrapper<value const> root):m_root{root}{}
+
+		serialize_result serialize(std::span<char> output_buffer, value const& val);
+
+	private:
+		std::reference_wrapper<value const> m_root;
+		std::stack<serailzer_context> m_contexts;
+	};
+}
+
+#endif
