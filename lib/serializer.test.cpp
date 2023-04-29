532dab89a87d9b3139dc5253331f15386cc7c957
Torbjörn Rathsman <mialsudril@gmail.com>
2023-04-29 10:44:24 +0200

    Add simple testcase for serializer

--- lib/serializer.test.cpp
+++ lib/serializer.test.cpp
@@ -3,3 +3,21 @@
 #include "./serializer.hpp"
 
 #include <testfwk/testfwk.hpp>
+
+TESTCASE(jopp_serializer_serialzie)
+{
+	jopp::object obj;
+
+	obj.insert("1. an array", jopp::array{});
+	obj.insert("2. an object", jopp::object{});
+	obj.insert("3. a number", 1.25);
+	obj.insert("4. null literal", jopp::null{});
+	obj.insert("5. a bool", true);
+	obj.insert("6. a string", "Hello, World");
+	
+	jopp::value val{std::move(obj)};
+	
+	jopp::serializer serializer{val};
+	
+	serializer.serialize(std::span<char>{});
+}
