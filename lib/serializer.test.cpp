03263110b7769729b014e1b5d1534659094d04e9
Torbjörn Rathsman <milasudril@gmail.com>
2023-05-05 20:07:46 +0200

    Make testcase more complex

--- lib/serializer.test.cpp
+++ lib/serializer.test.cpp
@@ -6,29 +6,52 @@
 
 TESTCASE(jopp_serializer_serialzie)
 {
-	jopp::array array;
-	array.push_back(0.5);
-	array.push_back("Some text");
-	array.push_back(false);
-	
-	jopp::object subobj;
-	subobj.insert("Key", "Value");
-	
-	jopp::object obj;
-	obj.insert("1. an array", std::move(array));
-	obj.insert("2. an object", std::move(subobj));
-	obj.insert("3. a number", 1.25);
-	obj.insert("4. null literal", jopp::null{});
-	obj.insert("5. a bool", true);
-	obj.insert("6. a string", "Hello, World");
-	
-	jopp::value val{std::move(obj)};
-	
+	jopp::object root;
+	root.insert("empty object", jopp::object{});
+	root.insert("empty array", jopp::array{});
+	jopp::object had;
+	jopp::array tightly;
+	jopp::array inner_array;
+	inner_array.push_back(4.0);
+	inner_array.push_back(2.0);
+	inner_array.push_back(3.0);
+	inner_array.push_back(1.0);
+	tightly.push_back(std::move(inner_array));
+	tightly.push_back("feet");
+	tightly.push_back(true);
+	tightly.push_back(2145719840.4312375);
+	tightly.push_back(-286229488.0);
+	tightly.push_back(true);
+	tightly.push_back(true);
+	jopp::object object_in_array;
+	object_in_array.insert("object in array", "bar");
+	tightly.push_back(std::move(object_in_array));
+	jopp::object object_with_literal_last;
+	object_with_literal_last.insert("object with literal last", jopp::null{});
+	tightly.push_back(std::move(object_with_literal_last));
+	had.insert("tightly", std::move(tightly));
+	had.insert("sound", false);
+	had.insert("eaten", false);
+	had.insert("pull", 1285774482.782745);
+	had.insert("long", -1437168945.8634152);
+	had.insert("independent", -1451031326.0);
+	jopp::object repeated_end;
+	repeated_end.insert("value", 46.0);
+	had.insert("repeated end", std::move(repeated_end));
+	root.insert("had", std::move(had));
+	root.insert("fireplace", 720535269.0),
+	root.insert("refused", "better"),
+	root.insert("wood", "involved"),
+	root.insert("without", true),
+	root.insert("it", false),
+	root.insert("testing null", jopp::null{}),
+	root.insert(R"(a key with esc seq
+	\foo")", R"(A value with esc seq
+	\foo")");
+	jopp::value val{std::move(root)};
+
 	jopp::serializer serializer{val};
-	
 	std::array<char, 1024> buffer{};
-	
 	serializer.serialize(buffer);
-	
-	printf("%s\n", buffer.data());;
+	printf("%s\n", buffer.data());
 }
