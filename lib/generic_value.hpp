c6bc4a3091171eea9300f9466967b8ca4ab06d0e
Torbjörn Rathsman <milasudril@gmail.com>
2026-06-25 15:44:41 +0200

    Cleanup

--- lib/generic_value.hpp
+++ lib/generic_value.hpp
@@ -3,6 +3,8 @@
 
 #include "./variant_utils.hpp"
 
+#include <memory>
+
 namespace jopp2
 {
 	template<
@@ -13,24 +15,28 @@ namespace jopp2
 	class generic_value
 	{
 	public:
-	private:
-		build_variant_t<
-			std::variant<
-				AssociativeContainerType<typename ValueTraits::key_type, generic_value>,
-				SequenceContainerType<generic_value>,
-				SequenceContainerType<AssociativeContainerType<typename ValueTraits::key_type, generic_value>>
+		using leaf_value_type = typename ValueTraits::leaf_value_type;
+		using key_type = typename ValueTraits::key_type;
+		using object = AssociativeContainerType<key_type, generic_value>;
+		using variant_type = concatenate_variants_t<
+			wrap_in_variant_t<leaf_value_type>,
+			wrap_in_variant_t<object>,
+			wrap_variant_element_t<
+				wrap_in_variant_t<leaf_value_type>, SequenceContainerType
 			>,
-			build_variant_t<
-				typename ValueTraits::leaf_value_type,
-				wrap_variant_element_t<
-					wrap_in_variant_t<typename ValueTraits::leaf_value_type>, SequenceContainerType
-				>
-			>
-		>
-		m_value;
+			wrap_in_variant_t<SequenceContainerType<object>>,
+			wrap_in_variant_t<SequenceContainerType<generic_value>>
+		>;
+
+		auto const& get() const
+		{ return m_value; }
+
+		static constexpr size_t first_sequence_type_index()
+		{ return std::variant_size_v<wrap_in_variant_t<leaf_value_type>> + 1; }
+
+	private:
+		variant_type m_value;
 	};
 }
-
-
 
 #endif
--- lib/generic_value.test.cpp
+++ lib/generic_value.test.cpp
@@ -10,7 +10,7 @@ namespace
 	struct my_value_traits_with_variant
 	{
 		using key_type = std::string;
-		using leaf_value_type = std::variant<std::string, double>;
+		using leaf_value_type = std::variant<int, double>;
 	};
 
 	struct my_value_traits_with_no_variant
@@ -22,6 +22,40 @@ namespace
 
 TESTCASE(jopp2_create_empty_value)
 {
-	jopp2::generic_value<std::flat_map, std::vector, my_value_traits_with_variant> val_1;
-	jopp2::generic_value<std::flat_map, std::vector, my_value_traits_with_no_variant> val_2;
+	using type_with_variant = jopp2::generic_value<std::flat_map, std::vector, my_value_traits_with_variant>;
+	EXPECT_EQ(
+		(std::is_same_v<
+			type_with_variant::variant_type,
+			std::variant<
+				int,
+				double,
+				type_with_variant::object,
+				std::vector<int>,
+				std::vector<double>,
+				std::vector<type_with_variant::object>,
+				std::vector<type_with_variant>
+			>
+		>),
+		true
+	);
+	EXPECT_EQ(std::is_constructible_v<type_with_variant>, true);
+	EXPECT_EQ(type_with_variant::first_sequence_type_index(), 3);
+
+	using type_with_no_variant = jopp2::generic_value<std::flat_map, std::vector, my_value_traits_with_no_variant>;
+		EXPECT_EQ(
+		(std::is_same_v<
+			type_with_no_variant::variant_type,
+			std::variant<
+				std::string,
+				type_with_no_variant::object,
+				std::vector<std::string>,
+				std::vector<type_with_no_variant::object>,
+				std::vector<type_with_no_variant>
+			>
+		>),
+		true
+	);
+	EXPECT_EQ(std::is_constructible_v<type_with_no_variant>, true);
+	EXPECT_EQ(type_with_no_variant::first_sequence_type_index(), 2);
+
 }
