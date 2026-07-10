21c0cb308bf402a55bca5a72dde35d96525f05f8
Torbjörn Rathsman <milasudril@gmail.com>
2026-07-10 21:15:02 +0200

    More tweaks to updater

--- lib/updater.hpp
+++ lib/updater.hpp
@@ -7,24 +7,40 @@
 
 namespace jopp2
 {
-
 	template <class T>
-	struct is_passed_in_register
+	struct pass_by_value
 	{
-		// 1. Must be trivial for the purpose of calls (per C++ ABI)
 		static constexpr bool is_trivial_abi = std::is_trivially_copyable_v<T> || std::is_reference_v<T>;
 
-		// 2. Size must be between 1 and 16 bytes (or be an empty struct/fundamental)
-		static constexpr bool valid_size = (sizeof(T) <= 16);
-
-		static constexpr bool value = is_trivial_abi && valid_size;
+	#ifndef __i386__
+		static constexpr bool platform_gate = (sizeof(T) <= 2*sizeof(void*));
+	#else
+		static constexpr bool platform_gate = std::is_fundamental_v<T>
+			|| std::is_pointer_v<T>
+			|| std::is_reference_v<T>
+			|| std::is_enum_v<T>;
+	#endif
+		static constexpr bool value = is_trivial_abi && platform_gate;
 	};
 
+	#ifdef __i386__
+		#define THISCALL [[gnu::thiscall]]
+	#else
+		#define THISCALL
+	#endif
+
 	template<class T>
-	inline constexpr auto is_passed_in_register_v = is_passed_in_register<T>::value;
+	inline constexpr auto pass_by_value_v = pass_by_value<T>::value;
 
 	template <class T>
-	using using_optimal_sink_t = std::conditional_t<is_passed_in_register_v<T>, T, T&&>;
+	using using_optimal_sink_t = std::conditional_t<
+		pass_by_value_v<T>,
+		T,
+		std::conditional_t<std::is_trivially_copyable_v<T>, T const&, T&&>
+	>;
+
+	template <class T>
+	using using_optimal_param_t = std::conditional_t<pass_by_value_v<T>, T, T const&>;
 
 	template<class... Types>
 	class updater
@@ -32,32 +48,35 @@ namespace jopp2
 	public:
 		template<class Sink, class UpdateTraits>
 		explicit updater(Sink& target, std::type_identity<UpdateTraits>):
-			m_handle{&target},
-			m_vtable{&s_vtable<Sink, UpdateTraits>}
+				m_handle{&target},
+				m_vtable{&s_vtable<Sink, UpdateTraits>}
 		{}
 
 		template<class SourceValue>
 		requires(
 			!std::is_lvalue_reference_v<SourceValue> ||
-			is_passed_in_register_v<std::remove_cvref_t<SourceValue>>
+			pass_by_value_v<std::remove_cvref_t<SourceValue>>
 		)
-		void update(SourceValue&& value) const
+		THISCALL void update_with(SourceValue&& value) const
 		{
 			using raw_type = std::remove_cvref_t<SourceValue>;
 			std::get<callback_type<raw_type>>(*m_vtable)(m_handle, std::forward<SourceValue>(value));
 		}
 
 		template<class SourceValue>
-		requires(!is_passed_in_register_v<std::remove_cvref_t<SourceValue>>)
-		void update(SourceValue const& value) const
+		requires(!pass_by_value_v<std::remove_cvref_t<SourceValue>>)
+		THISCALL void update_with(SourceValue const& value) const
 		{
 			using raw_type = std::remove_cvref_t<SourceValue>;
-			std::get<callback_type<raw_type>>(*m_vtable)(m_handle, raw_type{value});
+			if constexpr(std::is_trivially_copyable_v<std::remove_cvref_t<SourceValue>>)
+			{ std::get<callback_type<raw_type>>(*m_vtable)(m_handle, value); }
+			else
+			{ std::get<callback_type<raw_type>>(*m_vtable)(m_handle, raw_type{value}); }
 		}
 
 	private:
 		template<class T>
-		using callback_type = void (*)(void*, using_optimal_sink_t<T>);
+		using callback_type = void (*)(void*, using_optimal_sink_t<T>) THISCALL;
 		using vtable = std::tuple<callback_type<Types>...>;
 
 		void* m_handle;
@@ -65,8 +84,8 @@ namespace jopp2
 
 		template<class Sink, class UpdateTraits>
 		static constexpr vtable s_vtable{
-			[](void* target, using_optimal_sink_t<Types> value) {
-				if constexpr(is_passed_in_register_v<Types>)
+			[](void* target, using_optimal_sink_t<Types> value) THISCALL {
+				if constexpr(pass_by_value_v<Types> || std::is_trivially_copyable_v<Types>)
 				{ UpdateTraits::update(*static_cast<Sink*>(target), value); }
 				else
 				{ UpdateTraits::update(*static_cast<Sink*>(target), std::move(value)); }
--- lib/updater.test.cpp
+++ lib/updater.test.cpp
@@ -25,29 +25,29 @@ TESTCASE(jopp2_updater_update_value)
 		std::type_identity<test_updater_traits>{}
 	};
 
-	updater.update(124);
+	updater.update_with(124);
 	EXPECT_EQ(std::get<int>(value), 124);
 
-	updater.update(std::string{"Foobar"});
+	updater.update_with(std::string{"Foobar"});
 	EXPECT_EQ(std::get<std::string>(value), "Foobar");
 
 	std::string moved_from_value{"Foobar 2"};
-	updater.update(std::move(moved_from_value));
+	updater.update_with(std::move(moved_from_value));
 	EXPECT_EQ(std::get<std::string>(value), "Foobar 2");
 	// NOLINTNEXTLINE
 	EXPECT_EQ(moved_from_value.empty(), true);
 
 	int some_l_value = 123;
-	updater.update(some_l_value);
+	updater.update_with(some_l_value);
 	EXPECT_EQ(std::get<int>(value), 123);
 
 	std::string some_other_l_value{"Kaka"};
-	updater.update(some_other_l_value);
+	updater.update_with(some_other_l_value);
 	EXPECT_EQ(std::get<std::string>(value), "Kaka");
 	EXPECT_EQ(some_other_l_value, "Kaka");
 
 	std::string const yet_another_l_value{"Kaka 12"};
-	updater.update(yet_another_l_value);
+	updater.update_with(yet_another_l_value);
 	EXPECT_EQ(std::get<std::string>(value), "Kaka 12");
 	EXPECT_EQ(yet_another_l_value, "Kaka 12");
 }
