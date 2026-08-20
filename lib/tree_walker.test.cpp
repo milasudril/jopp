9e8a5481d0522b40c5ad61c13f4e39c45110d7da
Torbjörn Rathsman <milasudril@gmail.com>
2026-08-20 18:40:14 +0200

    Test value_visitation_context default state

--- lib/tree_walker.test.cpp
+++ lib/tree_walker.test.cpp
@@ -3,3 +3,10 @@
 #include "./tree_walker.hpp"
 
 #include <testfwk/testfwk.hpp>
+
+TESTCASE(jopp2_value_visitation_context_default_state)
+{
+	jopp2::value_visitation_context ctxt;
+	EXPECT_EQ(ctxt.is_first_node(), true);
+	EXPECT_EQ(ctxt.depth(), 0);
+}
